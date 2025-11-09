2098c455f09e5159d770a1c66880336d051fd720
domsrawr <jcdomingo8@up.edu.ph>
2025-11-10 01:26:16 +0800

    Update display.py

--- src/display.py
+++ src/display.py
@@ -1,9 +1,39 @@
-def convert_to_str(tiles):
-    """converts forest tiles to string as tiles is initially formatted as list
+"""Display module for handling all console outputs.
+
+This module is in charge of:
+- Rendering the forest grid
+- Displaying game instructions
+- Showing player status (items, mushrooms)
+- Win/lose screens
+"""
+import emoji
+
+ascii_to_emoji = {
+    'T': ":evergreen_tree:",
+    '.': "　",
+    'L': ":person:",
+    '+': ":mushroom:",
+    'R': ":rock:",
+    '~': ":blue_square:",
+    '_': ":white_large_square:",
+    'x': ":axe:",
+    '*': ":fire:"
+}
+
+def convert_to_emoji(grid):
+    """Convert and print the forest grid as emojis to the console.
+    
+    Args:
+        grid (list[list[str]]): 2D grid where each inner list is a row of tile characters
     """
-    print('\n'.join(list(''.join(row) for row in tiles)))
+    for row in grid:
+        map = ''.join(
+            (emoji.emojize(ascii_to_emoji.get(char, char), language='alias') + ' ') if char == 'R'
+            else emoji.emojize(ascii_to_emoji.get(char, char), language='alias') for char in row)
+        print(map)
 
 def display_movement_instructions():
+    """Displays movement and action controls"""
     print('''
     [W] to move up
     [A] to move left
@@ -11,45 +41,52 @@ def display_movement_instructions():
     [D] to move right
     [P] to pick up an item
     [!] to reset the stage
+          
+What's your move?
     ''')
 
 def tile_item(gamestate):
-    if gamestate['tile_item']:
-        print(f'The tile contains an {gamestate['tile_item']}!')
+    """Display what item (if any) is available on Larry's current tile.
+    
+    Args:
+        gamestate (dict): Current game state
+    """
+    tile_item = gamestate['tile_item']
+    if tile_item:
+        print(f'The tile contains {"an" if tile_item[0].lower() in "aeiou" else "a"} {tile_item}!')
     else:
         print(f'There is no item at the tile.')
     
 def display_mushroom_count(gamestate):
-    print(f'\n{gamestate['mushroom_count']} mushroom collected!')
+    """Display how many out of the max number of mushrooms have been collected.
+    
+    Args:
+        gamestate (dict): Current game state
+    """
+    print(f'\n{gamestate['mushroom_count']}/{gamestate['max_mushroom_count']} mushroom collected!')
 
 def display_item_holding(gamestate):
-    if gamestate['item_holding']:
-        print(f'You are holding a {gamestate['item_holding']}')
+    """Display what item (if any) Larry is currently holding.
+    
+    Args:
+        gamestate (dict): Current game state
+    """
+    item = gamestate['item_holding']
+    if item:
+        print(f'You are holding {"an" if item[0].lower() in "aeiou" else "a"} {item}.')
     else:
         print(f'You are not holding anything.')
 
-def win_mushroom_count(gamestate):
-    print(f"\nYou've collected {gamestate['mushroom_count']} mushrooms!")
-
 def win():
+    """Display victory screen."""
     print('''
-          _______                     _________ _        _ 
-|\     /|(  ___  )|\     /|  |\     /|\__   __/( (    /|( )
-( \   / )| (   ) || )   ( |  | )   ( |   ) (   |  \  ( || |
- \ (_) / | |   | || |   | |  | | _ | |   | |   |   \ | || |
-  \   /  | |   | || |   | |  | |( )| |   | |   | (\ \) || |
-   ) (   | |   | || |   | |  | || || |   | |   | | \   |(_)
-   | |   | (___) || (___) |  | () () |___) (___| )  \  | _ 
-   \_/   (_______)(_______)  (_______)\_______/|/    )_)(_)
+You won!
+Input '!' to play again!
     ''')
     
 def lose():
+    """Display game over screen."""
     print('''
-                
-/\_/\___  _   _  | | ___  ___  ___       
-\_ _/ _ \| | | | | |/ _ \/ __|/ _ \      
- / \ (_) | |_| | | | (_) \__ \  __/_ _ _ 
- \_/\___/ \__,_| |_|\___/|___/\___(_|_|_)
-                                         
-          ''')
-    
+You lost...
+Input '!' to play again!
+    ''')
